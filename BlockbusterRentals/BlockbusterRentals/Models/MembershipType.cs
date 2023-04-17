f190b304e7b0cb238d2d4649fb651338ba371868
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2023-04-17 17:54:47 -0600

    Modify MembershipType to have name, update each DB Entry, New Migrations, and change table to show Customer Membership Type

--- BlockbusterRentals/BlockbusterRentals/Models/MembershipType.cs
+++ BlockbusterRentals/BlockbusterRentals/Models/MembershipType.cs
@@ -8,6 +8,7 @@ namespace BlockbusterRentals.Models
     public class MembershipType
     { 
         public byte Id { get; set; } // for use in Foreign Key!
+        public string Name { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
