a36d3f2950d8a2dbe51e8719b101fc6b1dc3e621
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2023-04-17 11:46:40 -0600

    Basic MVC Video Rental site
    Following the Udemy course, I have completed a basic ASP.NET website, following the MVC convention. This commit shows a Customers and Movies page, each with their respective Models, Controllers, and Views

---
+++ BlockbusterRentals/BlockbusterRentals/Controllers/MoviesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlockbusterRentals.Models;
+using BlockbusterRentals.ViewModels;
+
+namespace BlockbusterRentals.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            // Create instance of a Movie Model
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+       
+
+        /*public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0}, sortBy={1}", pageIndex, sortBy));
+        }
+        */
+
+        // Pass a Model for the View to display
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+        // The Model to return to the View
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
