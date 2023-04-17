e9a57b07f40bb327ca22e9a68a2f77b3b1e79a30
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2023-04-17 16:59:30 -0600

    SQL Database Load Customer Data & Multi column Customer table

--- BlockbusterRentals/BlockbusterRentals/Controllers/CustomersController.cs
+++ BlockbusterRentals/BlockbusterRentals/Controllers/CustomersController.cs
@@ -4,34 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BlockbusterRentals.Models;
+using System.Data.Entity;
 
 namespace BlockbusterRentals.Controllers
 {
     public class CustomersController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            //var customers = GetCustomers();
+
+            //Now, initilize customers as a DB call
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList(); 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Williams" }
-            };
         }
     }
 }
