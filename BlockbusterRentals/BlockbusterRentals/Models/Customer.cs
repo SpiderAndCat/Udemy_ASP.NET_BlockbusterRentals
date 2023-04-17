f90c7237ec36218b31d90e8eb6f3e1743367dd88
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2023-04-17 14:41:15 -0600

    IMplemented MembershipType Model, as a foreign key reference to Customer

--- BlockbusterRentals/BlockbusterRentals/Models/Customer.cs
+++ BlockbusterRentals/BlockbusterRentals/Models/Customer.cs
@@ -9,5 +9,9 @@ namespace BlockbusterRentals.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter  { get; set; }
+        public MembershipType MembershipType { get; set; } // a Navigation property
+        public byte MembershipTypeID { get; set; } // a Navigation property, used as the foeign key bc convention
     }
 }
---
+++ BlockbusterRentals/BlockbusterRentals/Models/MembershipType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlockbusterRentals.Models
+{
+    public class MembershipType
+    { 
+        public byte Id { get; set; } // for use in Foreign Key!
+        public short SignUpFee { get; set; }
+        public byte DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+
+    }
+}
