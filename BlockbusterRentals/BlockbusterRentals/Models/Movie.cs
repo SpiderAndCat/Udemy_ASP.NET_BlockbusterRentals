3810f09505b5a75eaa68d599239d8da6ff38441f
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2023-04-17 18:37:49 -0600

    Added missing files for Movie Feature

--- BlockbusterRentals/BlockbusterRentals/Models/Movie.cs
+++ BlockbusterRentals/BlockbusterRentals/Models/Movie.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace BlockbusterRentals.Models
 {
@@ -9,5 +11,17 @@ namespace BlockbusterRentals.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        public string ReleaseDate { get; set; }
+
+        [Required]
+        public string DateAdded { get; set; }
+        
+        [Required]
+        public int NumberInStock { get; set; }
     }
 }
