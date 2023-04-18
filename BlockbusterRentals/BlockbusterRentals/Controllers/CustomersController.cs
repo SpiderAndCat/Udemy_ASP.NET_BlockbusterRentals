d9657a7045d85903b8d6069730156bf60ec67b96
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2023-04-18 12:44:32 -0600

    Added Form
    New Customer input form added, with text fields and a checkbox, both with are tied to a Model and the Customer Controller; has New customer View

--- BlockbusterRentals/BlockbusterRentals/Controllers/CustomersController.cs
+++ BlockbusterRentals/BlockbusterRentals/Controllers/CustomersController.cs
@@ -40,5 +40,10 @@ namespace BlockbusterRentals.Controllers
 
             return View(customer);
         }
+
+        public ActionResult New()
+        {
+            return View();
+        }
     }
 }
