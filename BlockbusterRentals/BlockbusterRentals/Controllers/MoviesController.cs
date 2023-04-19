3cb20b4c0e436c6fd9b3eec8b7993adbe14c8e16
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2023-04-18 20:59:48 -0600

    Genre now properly displayed on all Movie Views

--- BlockbusterRentals/BlockbusterRentals/Controllers/MoviesController.cs
+++ BlockbusterRentals/BlockbusterRentals/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -77,13 +78,13 @@ namespace BlockbusterRentals.Controllers
             //var customers = GetCustomers();
 
             //Now, initilize customers as a DB call
-            var movies = _context.Movies.ToList();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
             return View(movies);
         }
 
         public ActionResult Details(int id)
         {
-            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
             if (movie == null)
                 return HttpNotFound();
 
